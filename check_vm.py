194de850367b1363f7c518b455c2c82a9902a8b5
ulissesss <wareztown@inwind.it>
2023-12-08 12:32:19 -0800

    added comments

--- check_vm.py
+++ check_vm.py
@@ -4,6 +4,7 @@ import subprocess
 cmd = "prlctl list -a | grep running"
 ps = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
 output = ps.communicate()[0]
+#check if output is not empty
 if output is not None:
   print ("there are some vm running")
 else:
