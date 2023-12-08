1b3a0ae324142c00be77a659314aff017d8df0b2
ulissesss <wareztown@inwind.it>
2023-12-08 12:24:23 -0800

    creating if condition

--- check_vm.py
+++ check_vm.py
@@ -4,6 +4,7 @@ import subprocess
 cmd = "prlctl list -a | grep running"
 ps = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
 output = ps.communicate()[0]
-print(output)
-
-#prlctl start f83e88ba-d717-4b6c-bb80-92bec6d1bd58
+If ( output is not null):
+ print ("there are some vm running")
+else:
+ print ("no one is running")
